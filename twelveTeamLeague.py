5136daa1a5ae13b6985cd37f1af0f6869619bc69
Jeremy Hsu <jeremy@truex.com>
2016-09-14 11:31:45 -0700

    add owners

---
+++ twelveTeamLeague.py
@@ -0,0 +1,93 @@
+# Fill in the form below with info about your league managers
+# 'name' is there name, this will be a lookup key in the DB, so make sure its the same name you use when importing the data
+# 'nickname' is a shortened version of their name that will show up in the power rankings
+# 'picture' is a direct url to the picture you want to represent the manager in the power rankings
+# 'clubHouse' is the url link to the managers team page
+# 'lastYearPosition' is the position in which the player finished last year (ex. "1st") ("N/A" if no results from last year)
+
+managerInfo = [
+      {
+        'name': 'Grant',
+        'nickname': 'Grant',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=11',
+        'lastYearPosition': '9th'
+      },
+      {
+        'name': 'Jen Thai',
+        'nickname': 'Jen',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=12',
+        'lastYearPosition': 'N/A'
+      },
+      {
+        'name': 'Rajeev',
+        'nickname': 'Roger',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=3',
+        'lastYearPosition': 'N/A'
+      },
+      {
+        'name': 'Kerry',
+        'nickname': 'Kerry',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=10',
+        'lastYearPosition': '10th'
+      },
+      {
+        'name': 'Jeremy',
+        'nickname': 'Jeremy',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=2',
+        'lastYearPosition': '4th'
+      },
+      {
+        'name': 'Ron',
+        'nickname': 'Commish',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=1',
+        'lastYearPosition': '7th'
+      },
+      {
+        'name': 'Matt',
+        'nickname': 'Matt',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=7',
+        'lastYearPosition': 'N/A'
+      },
+      {
+        'name': 'Kin',
+        'nickname': 'Kin',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=9',
+        'lastYearPosition': '1st'
+      },
+      {
+        'name': ,'Ben'
+        'nickname': 'Blackie',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=8',
+        'lastYearPosition': '8th'
+      },
+      {
+        'name': 'Ed',
+        'nickname': 'Ed',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=6',
+        'lastYearPosition': '3rd'
+      },
+      {
+        'name': 'Richard',
+        'nickname': 'Richard',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=6',
+        'lastYearPosition': '2nd'
+      },
+      {
+        'name': 'Tony',
+        'nickname': 'Pony',
+        'picture': '',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=5',
+        'lastYearPosition': '6th'
+      }
+]
