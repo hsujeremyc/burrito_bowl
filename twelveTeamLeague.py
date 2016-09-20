2aadfcb7080f38a92ebf80509d539e8157a175e5
Jeremy Hsu <jeremy@truex.com>
2016-09-20 01:38:13 -0700

    update manager images

--- twelveTeamLeague.py
+++ twelveTeamLeague.py
@@ -9,84 +9,84 @@ managerInfo = [
       {
         'name': 'Grant',
         'nickname': 'Grant',
-        'picture': '',
+        'picture': 'http://i.imgur.com/YFsxY6d.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=11',
         'lastYearPosition': '9th'
       },
       {
         'name': 'Jen',
         'nickname': 'Jen',
-        'picture': '',
+        'picture': 'http://i.imgur.com/SSNCQj4.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=12',
         'lastYearPosition': 'N/A'
       },
       {
         'name': 'Rajeev',
         'nickname': 'Roger',
-        'picture': '',
+        'picture': 'http://i.imgur.com/gIR8WGq.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=3',
         'lastYearPosition': 'N/A'
       },
       {
         'name': 'Kerry',
         'nickname': 'Kerry',
-        'picture': '',
+        'picture': 'http://i.imgur.com/lJKrXf4.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=10',
         'lastYearPosition': '10th'
       },
       {
         'name': 'Jeremy',
         'nickname': 'Jeremy',
-        'picture': '',
+        'picture': 'http://i.imgur.com/gj4BBhd.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=2',
         'lastYearPosition': '4th'
       },
       {
         'name': 'Ron',
         'nickname': 'Commish',
-        'picture': '',
+        'picture': 'http://i.imgur.com/jcPemZ7.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=1',
         'lastYearPosition': '7th'
       },
       {
         'name': 'Matt',
         'nickname': 'Matt',
-        'picture': '',
+        'picture': 'http://i.imgur.com/xieuSIt.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=7',
         'lastYearPosition': 'N/A'
       },
       {
         'name': 'Kin',
         'nickname': 'Kin',
-        'picture': '',
+        'picture': 'http://i.imgur.com/asfez8i.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=9',
         'lastYearPosition': '1st'
       },
       {
         'name': 'Ben',
         'nickname': 'Blackie',
-        'picture': '',
+        'picture': 'http://i.imgur.com/0hLjjxZ.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=8',
         'lastYearPosition': '8th'
       },
       {
         'name': 'Ed',
         'nickname': 'Ed',
-        'picture': '',
+        'picture': 'http://i.imgur.com/aVLkjK4.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=6',
         'lastYearPosition': '3rd'
       },
       {
         'name': 'Richard',
         'nickname': 'Richard',
-        'picture': '',
+        'picture': 'http://i.imgur.com/jYyrM0T.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=6',
         'lastYearPosition': '2nd'
       },
       {
         'name': 'Tony',
         'nickname': 'Pony',
-        'picture': '',
+        'picture': 'http://i.imgur.com/wqEDbjs.jpg',
         'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=1160737&teamId=5',
         'lastYearPosition': '6th'
       }
