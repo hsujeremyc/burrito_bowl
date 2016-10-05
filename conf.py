d3ddfa9a688204d6ee8c5d0863d8749d1f6bd865
Jeremy Hsu <jeremy@truex.com>
2016-10-04 19:36:16 -0700

    week 4

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@ from twelveTeamLeague import managerInfo
 
 #Date Settings
 year = 2016
-week = 3 # Values - 'Preseason', 1, 2, 3 ... 13
+week = 4 # Values - 'Preseason', 1, 2, 3 ... 13
 
 firstYear = 2016
 yearsCompleted = []
